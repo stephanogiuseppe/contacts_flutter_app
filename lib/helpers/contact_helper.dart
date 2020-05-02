8b7b0a92cd68cd744afeac08cba3022dea5a576d
stephanogiuseppe <stephano.grp@gmail.com>
2020-05-01 23:51:38 -0300

    chore(contact_helper): db connection created

---
+++ lib/helpers/contact_helper.dart
@@ -0,0 +1,81 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+final String contactTable = "contactTable";
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
+final String registerColumn = "registerColumn";
+final String cardColumn = "cardColumn";
+
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initDb();
+    }
+    return _db;
+  }
+
+  Future<Database> initDb() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, "contacts.db");
+
+    return await openDatabase(path, version: 1, onCreate: (Database db, newerVersion) async {
+      await db.execute("CREATE TABLE $contactTable("
+          "$idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
+          "$phoneColumn TEXT, $imgColumn TEXT, $registerColumn TEXT, $cardColumn TEXT)");
+    });
+  }
+}
+
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+  String register;
+  String card;
+
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+    register = map[registerColumn];
+    card = map[cardColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img,
+      registerColumn: register,
+      cardColumn: card
+    };
+
+    if (id != null) {
+      map[idColumn] = id;
+    }
+
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, $email: $email, phone: $phone";
+  }
+}
